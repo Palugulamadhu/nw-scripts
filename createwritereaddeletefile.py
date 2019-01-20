a968e150cee03b2214b2a5bddb4b8cf30edc62b5
Palugulamadhu <46858714+Palugulamadhu@users.noreply.github.com>
2019-01-20 12:25:34 +0530

    Add files via upload

---
+++ createwritereaddeletefile.py
@@ -0,0 +1,41 @@
+#will create new madhu.txt file
+f = open("madhu.txt" , "x")
+
+#open the madhu.txt file in write mode
+f = open("madhu.txt","w")
+print ("Erasing content in file!!")
+f.truncate() #will erase if any data exists inside filename
+
+print("please enter the new lines:")
+
+#script asks for data to be written to file
+line1 = input("line1: ")
+line2 = input("line2: ")
+line3 = input("line3: ")
+
+print("Going to write new 3 lines to new.txt file:")
+
+f.write(line1)
+f.write("\n")
+f.write(line2)
+f.write("\n")
+f.write(line3)
+f.write("\n")
+f.write("**** End of the file ****")
+
+print("Now we'll readout new written lines")
+
+#prints the output from madhu.txt file
+f = open("madhu.txt" , "r")
+print(f.read())
+f.close()
+
+#deletes the file madhu.txt once data is written
+import os
+if os.path.exists("madhu.txt"):
+    os.remove("madhu.txt")
+else:
+    print("The file doesn't exists!!!")
+
+
+
